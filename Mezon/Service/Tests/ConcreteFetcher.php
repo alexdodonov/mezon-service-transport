30f588d85556d6e15d12a33c616adb6c4c446cda
gdever <alexey@dodonov.pro>
2020-12-18 12:48:59 +0300

    bugfix/Some unit-tests were fixed

--- Mezon/Service/Tests/ConcreteFetcher.php
+++ Mezon/Service/Tests/ConcreteFetcher.php
@@ -5,9 +5,14 @@ use Mezon\Transport\RequestParamsInterface;
 
 class ConcreteFetcher implements RequestParamsInterface
 {
-    
+
     public function getParam($param, $default = false)
     {
         return 1;
+    }
+
+    public function wasSubmitted(string $param): bool
+    {
+        return false;
     }
 }
